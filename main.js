8c695b508b46c5a658fea349522d5c5f1e0ec0d8
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2025-07-23 17:42:02 +0200

    added rendering tasks, saving and loading into local storage

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-import Task from "./item";
+import Task from "./item.js";
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 const addTask = () => {
     const id = Date.now();
@@ -7,9 +9,32 @@ const addTask = () => {
     const description = document.querySelector("#description");
     const newTask = new Task(id, title.value, due.value, description.value, false);
 
-    console.log(newTask);
+    tasks.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+const renderTasks = () => {
+    const contentDiv = document.querySelector("#content");
+    contentDiv.innerHTML = "";
+    tasks.forEach(task => {
+        const parentDiv = document.createElement("div");
+        parentDiv.classList.add("task")
+        const title = document.createElement("h1");
+        title.innerHTML = task.title;
+        const due = document.createElement("h3");
+        due.innerHTML = `Due: ${task.due}`;
+        const description = document.createElement("p");
+        description.innerHTML = task.description;
+        parentDiv.appendChild(title);
+        parentDiv.appendChild(due);
+        parentDiv.appendChild(description);
+        contentDiv.appendChild(parentDiv);
+    });
 }
 
 document.querySelector("#add").addEventListener("click", () => {
     addTask();
+    renderTasks();
 })
+
+renderTasks();
