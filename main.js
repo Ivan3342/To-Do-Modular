bff1679397eb07a969e6c6459a32ccf5a023997d
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2025-07-23 18:43:00 +0200

    added the ability to complete tasks and remove them

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Task from "./item.js";
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let completedTasks = JSON.parse(localStorage.getItem("completedTasks")) || [];
 
 const addTask = () => {
     const id = Date.now();
@@ -10,7 +11,7 @@ const addTask = () => {
     const newTask = new Task(id, title.value, due.value, description.value, false);
 
     tasks.push(newTask);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    updateTasks();
 }
 
 const renderTasks = () => {
@@ -18,18 +19,47 @@ const renderTasks = () => {
     contentDiv.innerHTML = "";
     tasks.forEach(task => {
         const parentDiv = document.createElement("div");
+        parentDiv.id = task.id;
         parentDiv.classList.add("task")
+
         const title = document.createElement("h1");
         title.innerHTML = task.title;
         const due = document.createElement("h3");
         due.innerHTML = `Due: ${task.due}`;
         const description = document.createElement("p");
         description.innerHTML = task.description;
+
+        const detailsButton = document.createElement("button");
+        detailsButton.innerHTML = "Details";
+        const completeButton = document.createElement("button");
+        completeButton.innerHTML = "Complete";
+
+        completeButton.addEventListener("click", completeTask);
+
         parentDiv.appendChild(title);
         parentDiv.appendChild(due);
         parentDiv.appendChild(description);
+        parentDiv.appendChild(detailsButton)
+        parentDiv.appendChild(completeButton)
         contentDiv.appendChild(parentDiv);
     });
+}
+
+const completeTask = (e) => {
+    const selectedTask = e.target.parentElement;
+    for (const task of tasks) {
+        if(selectedTask.id == task.id){
+            completedTasks.push(task);
+            tasks.splice(task)
+            selectedTask.remove();
+            updateTasks();
+            break;
+        }
+    }
+}
+
+const updateTasks = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 document.querySelector("#add").addEventListener("click", () => {
